159f5644934d3c9fc2c3c1fb00a21f63333ba172
WitheringAway <124115470+notwithering@users.noreply.github.com>
2026-02-28 19:17:25 -0500

    fix using font.Height instead of 3 for colBit iteration

--- gfx/draw.go
+++ gfx/draw.go
@@ -44,7 +44,7 @@ func (b *Buffer) DrawChar(x, y int, col color.Color, font *font.Font, character 
 	b8b := uint8(b8 >> 8)
 
 	for rowNum, row := range glyph {
-		for colBit := range font.Width {
+		for colBit := range 3 {
 			if row&(1<<(2-colBit)) != 0 {
 				px := x + colBit
 				py := y + rowNum + font.YOffset
